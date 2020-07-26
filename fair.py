b025fca2549cdff19f06562d8b6c771252686501
Cyber MacGeddon <cybermaggedon@gmail.com>
2020-07-26 10:52:38 +0100

    Broke the "curves" API call into loss, pdf, risk, summary

--- fair.py
+++ fair.py
@@ -6,6 +6,10 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
+lef_points = 50
+risk_bins = 25
+pdf_points = 50
+
 # all returns map of models
 def load_model(spec, all):
 
@@ -79,10 +83,6 @@ def summary(model):
 
 def curves(model, all_models):
 
-    lef_points = 50
-    pdf_points = 50
-    risk_bins = 25
-
     output = {}
 
     results = model.export_results()
@@ -141,4 +141,118 @@ def curves(model, all_models):
 
     return output
 
+def loss(model, all_models):
 
+    output = {}
+
+    results = model.export_results()
+
+    for name in all_models:
+
+        model = all_models[name]
+
+        model_out = {}
+        
+        risk = model.export_results()["Risk"]
+        risk_max = risk.max()
+
+        space = pd.Series(np.linspace(0, risk_max, lef_points))
+
+        prob = space.map(lambda x: stats.percentileofscore(risk, x))
+        loss = space.map(lambda value: (value < risk).mean()) * 100
+
+        model_out["prob"] = [
+            [space[v], prob[v]] for v in range(0, len(space))
+        ];
+
+        model_out["loss"] = [
+            [space[v], loss[v]] for v in range(0, len(space))
+        ];
+
+        output[name] = model_out
+
+    return output
+
+
+
+def summary(model, all_models):
+
+    output = {}
+
+    results = model.export_results()
+
+    for name in all_models:
+
+        model = all_models[name]
+
+        model_out = {}
+        
+        risk = model.export_results()["Risk"]
+        risk_max = risk.max()
+
+        model_out["summary"] = {
+            'mean': risk.mean(),
+            'min': risk.min(),
+            'max': risk.max(),
+            'stddev': risk.std()
+        }
+
+        output[name] = model_out
+
+    return output
+
+
+
+def pdf(model, all_models):
+
+    output = {}
+
+    results = model.export_results()
+    all_risk = results["Risk"]
+    all_max = all_risk.max()
+
+    for name in all_models:
+
+        model = all_models[name]
+
+        model_out = {}
+        
+        risk = model.export_results()["Risk"]
+        risk_max = risk.max()
+
+        beta_curve = stats.beta(*stats.beta.fit(risk))
+
+        pdf_space = np.linspace(0, risk_max, pdf_points)
+        pdf = beta_curve.pdf(pdf_space)
+
+        model_out["pdf"] = [
+            [pdf_space[v], pdf[v]]
+            for v in range(0, len(pdf_space))
+        ]
+
+        output[name] = model_out
+
+    return output
+
+def risk(model, all_models):
+
+    output = {}
+
+    for name in all_models:
+
+        model = all_models[name]
+
+        model_out = {}
+        
+        risk = model.export_results()["Risk"]
+
+        hist = np.histogram(risk, bins=risk_bins)
+
+        model_out["risk"] = [
+            [hist[1][v], int(hist[0][v])]
+            for v in range(0, risk_bins)
+        ];
+
+        output[name] = model_out
+
+    return output
